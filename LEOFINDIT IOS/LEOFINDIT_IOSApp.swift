628bfa635f7771dc69204ab01acc5e25b0a6f1b3
Niel Kishor Patel <nielpatel1212@outlook.com>
2024-10-30 10:29:21 -0400

    Initial Commit

---
+++ LEOFINDIT IOS/LEOFINDIT_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LEOFINDIT_IOSApp.swift
+//  LEOFINDIT IOS
+//
+//  Created by Niel Patel on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct LEOFINDIT_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
